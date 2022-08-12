a54f0820e9792b1c4143b7993c5895c33bd09ad8
Anna Lyn Williams <110787308+annalynwilliams@users.noreply.github.com>
2022-08-12 14:11:59 -0400

    Update Sals-Shipping.py

--- Sals-Shipping.py
+++ Sals-Shipping.py
@@ -7,8 +7,23 @@
 #Sal’s Shippers has several different options for a customer to ship their package:
 
 # 1) Ground Shipping, which is a small flat charge plus a rate based on the weight of your package.
+    # Weight of Package	Price per Pound	Flat Charge
+    # 2 lb or less	$1.50	$20.00
+    # Over 2 lb but less than or equal to 6 lb	$3.00	$20.00
+    # Over 6 lb but less than or equal to 10 lb	$4.00	$20.00
+    # Over 10 lb	$4.75	$20.00
+  
 # 2) Ground Shipping Premium, which is a much higher flat charge, but you aren’t charged for weight.
+    # Flat charge: $125.00
+  
 # 3) Drone Shipping (new), which has no flat charge, but the rate based on weight is triple the rate of ground shipping.
+    # Weight of Package	Price per Pound	Flat Charge
+    # 2 lb or less	$4.50	$0.00
+    # Over 2 lb but less than or equal to 6 lb	$9.00	$0.00
+    # Over 6 lb but less than or equal to 10 lb	$12.00	$0.00
+    # Over 10 lb	$14.25	$0.00
+
+# Code:
 
 weight = 41.5 #in lbs
 print('Weight (lbs): ', weight)
