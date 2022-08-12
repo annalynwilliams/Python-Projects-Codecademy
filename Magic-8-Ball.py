86b0d659ef5ddc1a1905ba1fdc94c369deab86d1
Anna Lyn Williams <110787308+annalynwilliams@users.noreply.github.com>
2022-08-12 14:14:06 -0400

    Update Magic-8-Ball.py

--- Magic-8-Ball.py
+++ Magic-8-Ball.py
@@ -1,3 +1,8 @@
+# The Magic 8-Ball is a popular toy developed in the 1950s for fortune-telling or advice seeking.
+# Write a magic8.py Python program that can answer any “Yes” or “No” question with a different fortune each time it executes.
+
+# Code:
+
 import random
 random_number = random.randint(1, 9)
 name = 'User'
