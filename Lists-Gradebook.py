31a0bbdf1641a560942714a6852e0950159f8e9d
Anna Lyn Williams <annawilliams2022@icloud.com>
2022-08-12 14:32:53 -0400

    Create Lists-Gradebook.py

---
+++ Lists-Gradebook.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+last_semester_gradebook = [["politics", 80], ["latin", 96], ["dance", 97], ["architecture", 65]]
+
+# Your code below: 
+subjects = [["physics"],
+["calculus"],
+["poetry"],
+["history"]]
+grades = [98,97,85,88]
+gradebook=[["physics",98],['calculus',97],['poetry',85],['history',88]]
+print(gradebook)
+gradebook.append(['computer_science',100])
+gradebook.append(['visual_arts',93])
+gradebook[-1][-1] = 93+5
+gradebook[-1].remove(98)
+gradebook[-1].append('Pass')
+print(gradebook)
+full_gradebook = last_semester_gradebook + gradebook
+print(full_gradebook)
